af58448403a949af7c6c8de289f89204d033b78b
dmedvik481 <dmedvik1@cougars.ccis.edu>
2025-12-02 19:34:28 -0600

    Add files via upload

---
+++ HaircutBookingSystem/Services/SmtpEmailSender.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+
+namespace HaircutBookingSystem.Services
+{
+    public class SmtpEmailSender : IEmailSender
+    {
+        private readonly SmtpOptions _options;
+        public SmtpEmailSender(SmtpOptions options)
+        {
+            _options = options;
+        }
+
+
+        public async Task SendAsync(string toEmail, string subject, string htmlBody)
+        {
+            using var client = new SmtpClient(_options.Host, _options.Port)
+            {
+                EnableSsl = _options.UseSsl,
+                Credentials = new NetworkCredential(_options.Username, _options.Password)
+            };
+
+
+            var from = new MailAddress(_options.FromAddress, _options.FromName);
+            var to = new MailAddress(toEmail);
+
+
+            using var message = new MailMessage(from, to)
+            {
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true
+            };
+
+
+            await client.SendMailAsync(message);
+        }
+    }
+}
