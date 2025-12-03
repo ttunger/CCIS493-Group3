ef5e2fa632db3ecf66a0b74e897ad68148781a4c
Matth <Matth@BUSBY-DENPC>
2025-12-02 19:30:09 -0600

    matin repo

---
+++ HaircutBookingSystem/Data/SalonContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HaircutBookingSystem.Data
+{
+    public class SalonContext : DbContext
+    {
+        public SalonContext(DbContextOptions<SalonContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Stylist> Stylists => Set<Stylist>();
+        public DbSet<AppointmentType> AppointmentTypes => Set<AppointmentType>();
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+        public DbSet<StylistNote> StylistNotes => Set<StylistNote>();
+    }
+}
---
+++ HaircutBookingSystem/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace HaircutBookingSystem.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options) { }
+
+        public DbSet<Service> Services => Set<Service>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Service>()
+                .Property(s => s.Price)
+                .HasColumnType("decimal(10,2)");
+
+            modelBuilder.Entity<Service>()
+                .HasIndex(s => s.Name)
+                .IsUnique(); // Optional: prevent duplicate names
+        }
+    }
+}
