0d9da1c83fd32a9be7ffa31dbf4c7ed357e84a9c
dmedvik481 <dmedvik1@cougars.ccis.edu>
2025-12-09 23:11:49 -0600

    Add files via upload

--- HaircutBookingSystem/Program.cs
+++ HaircutBookingSystem/Program.cs
@@ -1,11 +1,26 @@
 using HaircutBookingSystem.Models;
+using Microsoft.AspNetCore.Identity;
 using HaircutBookingSystem.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connection = $"Server=(localdb)\\mssqllocaldb;Database=HaircutBookingDb;Trusted_Connection=True;";
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
 // Register services (must happen before Build)
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 // Bind SMTP settings from configuration
 var smtpSection = builder.Configuration.GetSection("Smtp");
@@ -33,6 +48,7 @@ app.UseAuthentication(); // if using Identity
 app.UseAuthorization();
 
 // Default route goes to Homepage
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
